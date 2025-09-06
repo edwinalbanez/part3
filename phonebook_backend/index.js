917b636b2edfa11e246300b4cea42ce4cc4b1b77
edwinalbanez <edwinalbanez26@gmail.com>
2025-09-06 10:53:31 -0600

    port

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -94,7 +94,8 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running in port ${3001}`);
 });
