4731007842c13bfc659d752e0936f906d2f6b198
edwinalbanez <edwinalbanez26@gmail.com>
2025-09-05 21:01:08 -0600

    3.5: phonebook backend, step 5

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json())
 
-
-let persons = [
+let people = [
   {
     "id": 1,
     "name": "Arto Hellas",
@@ -27,19 +27,19 @@ let persons = [
 ];
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  response.json(people);
 });
 
 app.get('/info', (request, response) => {
   response.send(`
-    Phonebook has info for ${persons.length} people. <br />
+    Phonebook has info for ${people.length} people. <br />
     ${Date()}
   `)
 });
 
 app.get('/api/persons/:id',  (request, response) => {
   const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
+  const person = people.find(person => person.id === id);
 
   if (!person) {
     return response.status(404).end();
@@ -50,14 +50,33 @@ app.get('/api/persons/:id',  (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
-  const personToDelete = persons.find(person => person.id === id);
+  const personToDelete = people.find(person => person.id === id);
 
   if (!personToDelete) {
     return response.status(404).end();
   }
 
-  persons = persons.filter(person => person.id !== id);
+  people = people.filter(person => person.id !== id);
   response.status(204).end();
+})
+
+const getNewId = () => {
+  return Math.floor((Math.random() * 1000));
+}
+
+app.post('/api/persons', (request, response) => {
+  const { name, number } = request.body
+
+  if (!(name && number)) {
+    return response.status(400).json({
+      error: "name and number are required."
+    });
+  }
+
+  const newPerson = { id: getNewId(), name, number };
+  people = people.concat(newPerson);
+  response.json(newPerson);
+
 })
 
 const PORT = 3001;
